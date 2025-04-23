d5f9333338613a6e1b9170f13e6bdc73220ee3a7
Christophe <christophe.lopez@acseo-conseil.fr>
2025-04-23 15:09:42 +0200

    feat: ajoute la formule acseo-commit v1.0.0

---
+++ Formula/acseo-commit.rb
@@ -0,0 +1,18 @@
+class AcseoCommit < Formula
+    desc "Outil de génération de messages de commit selon la convention ACSEO"
+    homepage "https://github.com/ProToolsHub/ACSEO-COMMIT"
+    url "https://github.com/ProToolsHub/ACSEO-COMMIT/releases/download/v1.0.0/acseo-commit-1.0.0.tar.gz"
+    sha256 "885ad58bb16bd92c5d23797dbcb06589012a65032d018827d09bf57b9931308e"
+    license "MIT"
+  
+    depends_on "node"
+  
+    def install
+      system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+      bin.install_symlink Dir["#{libexec}/bin/*"]
+    end
+  
+    test do
+      assert_match "ACSEO Commit", shell_output("#{bin}/acseo-commit --version || true")
+    end
+  end
