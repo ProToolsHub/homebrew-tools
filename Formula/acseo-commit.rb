1b221b7f41c7e45f36ed70cc876602780d94db97
Christophe <christophe.lopez@acseo-conseil.fr>
2025-04-23 17:37:07 +0200

    v1.0.1

--- Formula/acseo-commit.rb
+++ Formula/acseo-commit.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AcseoCommit < Formula
   desc "Outil de génération de messages de commit selon la convention ACSEO"
   homepage "https://github.com/ProToolsHub/ACSEO-COMMIT"
-  url "https://raw.githubusercontent.com/ProToolsHub/homebrew-tools/main/archives/acseo-commit-1.0.0.tar.gz"
-  sha256 "885ad58bb16bd92c5d23797dbcb06589012a65032d018827d09bf57b9931308e"
+  url "https://raw.githubusercontent.com/ProToolsHub/homebrew-tools/main/archives/acseo-commit-1.0.1.tar.gz"
+  sha256 "34a105a3de1e0c676d0b2d41b1d8efffde6eb668c60b5ba0fd1aaee2e05fb5ed"
   license "MIT"
 
   depends_on "node"
